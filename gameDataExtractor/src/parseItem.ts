b7e882c58e5631984170ec8456b246143ecf83bd
vlopp <vloppworks@gmail.com>
2019-12-31 12:05:56 +0100

    All item collisions and item names are fixed now.

--- gameDataExtractor/src/parseItem.ts
+++ gameDataExtractor/src/parseItem.ts
@@ -1,11 +1,29 @@
-function encodeName(rawItemData) {
+function encodedName(rawItemData) {
   const simpleReplacements = {
     "<3": "Less_Than_Three",
     "Cat-o-nine-tails": "Cat-O-Nine-Tails",
-    "Humbleing_Bundle":"Humbling_Bundle",
-    "E._Coli": "E_Coli",
-    "Spiderbaby":"Spider_Baby",
-
+    "Humbleing Bundle": "Humbling_Bundle",
+    "E. Coli": "E_Coli",
+    Spiderbaby: "Spider_Baby",
+    "Tooth Picks": "Toothpicks",
+    "Contract from Below": "Contract_From_Below",
+    "BOGO Bombs": "Bogo_Bombs",
+    "Pay To Play": "Pay_to_Play",
+    "Maw Of The Void": "Maw_of_the_Void",
+    "Spear Of Destiny": "Spear_of_Destiny",
+    "Crown Of Light": "Crown_of_Light",
+    "Dark Princes Crown": "Dark_Prince's_Crown",
+    "BackStabber": "Backstabber",
+    "Forever alone": "Forever_Alone",
+    "???'s Only Friend": "%3F%3F%3F%27s_Only_Friend",
+    "Lil Loki": "Lil%27_Loki",
+    "Teleport!": "Teleport",
+    "Shoop da Whoop!": "Shoop_Da_Whoop!",
+    "We Need To Go Deeper!": "We_Need_to_Go_Deeper!",
+    "Telepathy For Dummies": "Telepathy_for_Dummies",
+    "D infinity": "D_Infinity",
+    "???'s Soul": "%3F%3F%3F%27s_Soul",
+    "Pay To Win": "Pay_to_Win"
   };
 
   switch (true) {
@@ -26,17 +44,32 @@ function encodeName(rawItemData) {
   }
 }
 
-export function parseItem(
-    itemData,
-    spriteSheetOffsets
-) {
+function readableName(rawItemData) {
+  const simpleReplacements = {
+    Spiderbaby: "Spider Baby"
+  };
+
+  switch (true) {
+    case Object.keys(simpleReplacements).includes(rawItemData.name):
+      return simpleReplacements[rawItemData.name];
+    default:
+      return rawItemData.name;
+  }
+}
+
+export function parseItem(itemData, spriteSheetOffsets) {
   return {
-    id: itemData.type !== "trinket" ? parseInt(itemData.id) : parseInt(itemData.id) + 6666,
-    readableName: itemData.name,
-    encodedName: encodeName(itemData),
+    id:
+      itemData.type !== "trinket"
+        ? parseInt(itemData.id)
+        : parseInt(itemData.id) + 6666,
+    readableName: readableName(itemData),
+    encodedName: encodedName(itemData),
     type: itemData.type.replace(/^\w/, chr => chr.toUpperCase()),
     shortDescription: itemData.description,
     longDescription: {},
-    offset: spriteSheetOffsets ? spriteSheetOffsets[itemData.gfx.toLowerCase()] : null
+    offset: spriteSheetOffsets
+      ? spriteSheetOffsets[itemData.gfx.toLowerCase()]
+      : null
   };
 }
